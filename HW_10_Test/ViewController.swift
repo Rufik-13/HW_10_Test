05ec4623cba52c8bf0c59954ef44bfcbb1f642be
Ruf <rmedetbayev@gmail.com>
2023-06-01 11:17:30 +0600

    Delete commint

--- HW_10_Test/ViewController.swift
+++ HW_10_Test/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  HW_10_Test
-//
-//  Created by Ruslan Medetbayev on 01.06.2023.
-//
+
 
 import UIKit
 
@@ -11,9 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
