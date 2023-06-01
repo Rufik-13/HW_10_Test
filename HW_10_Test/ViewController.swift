d6f69dcbd61a10ef94471ec88f3e2a6f78158a85
Ruf <rmedetbayev@gmail.com>
2023-06-01 10:57:37 +0600

    Initial Commit

---
+++ HW_10_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_10_Test
+//
+//  Created by Ruslan Medetbayev on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
